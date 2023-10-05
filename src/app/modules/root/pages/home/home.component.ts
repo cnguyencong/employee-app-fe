db4f09d64321e3c988732d1ddbfdf852488b6a3f
Tai Vo <tantaivo74@gmail.com>
2023-10-05 15:55:32 +0700

    feat: testing @ngxs/form-plugin

--- src/app/modules/root/pages/home/home.component.ts
+++ src/app/modules/root/pages/home/home.component.ts
@@ -1,12 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { UpdateFormValue } from '@ngxs/form-plugin';
+import { Store } from '@ngxs/store';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
+  newNovelForm = new FormGroup({
+    novelName: new FormControl('')
+  });
 
-  ngOnInit() {
+  constructor(private store: Store) {}
+
+  ngOnInit() {}
+
+  onSubmit(formData: FormGroup) {
+    const { value: user, status } = formData;
+    if (status === 'VALID') {
+      // do something
+    }
   }
 
+  onReset() {
+    this.store.dispatch(
+      new UpdateFormValue({
+        value: { novelName: '' },
+        path: 'novels.newNovelForm',
+      })
+    );
+  }
 }
