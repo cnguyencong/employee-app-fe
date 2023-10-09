d0b250988efa8bbc4d5bc381fa340c669fcb5fff
docaotu <openwt@OpenWTs-MBP.openwtdev>
2023-10-09 17:12:16 +0700

    My CV page

---
+++ src/app/modules/root/pages/profile/my-cv/dummy.data.ts
@@ -0,0 +1,8 @@
+export const dummyUserProfile = {
+  full_name : 'Trung Nguyen',
+  photo: 'https://www.w3schools.com/w3images/avatar2.png',
+  position : 'BE developer',
+  cv : 'https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf',
+  qr_code : 'https://img.freepik.com/premium-vector/qr-code-sample-smartphone-scanning-qr-code-icon-flat-design-stock-vector-illustration_550395-108.jpg',
+  modified_at : '14:00:00 05/10/2023'
+}
